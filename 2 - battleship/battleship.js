60e9f15de4568d31ccd60fa02fb66be8f4bc6fcf
FredMSJ <fmdsj13@gmail.com>
2020-12-04 23:00:54 -0300

    validacao do palpite

--- 2 - battleship/battleship.js
+++ 2 - battleship/battleship.js
@@ -8,5 +8,20 @@ var guesses = 0;
 
 var isSunk = false;
 
-//continuar pag 51.
-//Próxima execução: criar um loop e obter um palpite do usuário.
+while (isSunk == false) {
+  guess = prompt("Ready, aim, fire! (enter a number 0-6) :");
+
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a number between 0 and 6!");
+  } else {
+    guesses = guesses + 1;
+
+    if (guess == location1 || guess == location2 || guess == location3) {
+      hits = hits + 1;
+    }
+  }
+}
+
+//adicionado validacao do palpite.
+//adicionado incremento a variavel "hits" se o palpite for correto.
+//continuar na pagina 56
